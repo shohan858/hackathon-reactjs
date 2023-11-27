b5f5c585a3752446c0d7e4e498acb0f8bf4989f9
Muhammad Furqon <arkaisfurqon@gmail.com>
2023-11-27 15:15:17 +0700

    Muhammad Furqon Senin, 27/11/2023 first commit

---
+++ MuhammadFurqon/video3/text.md
@@ -0,0 +1,2 @@
+Video 3 berisi cara import react.js jika tidak menginstall react js sejak awal
+walaupun metode ini lebih rumit daripada membuat app react terlebih dahulu
---
+++ MuhammadFurqon/video6/src/component/YouTubeComp/YouTubeComp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './YouTubeComp.css';
+
+const YouTubeComp = (props) => {
+    return (
+        <div className="youtube-wrapper">
+            <div className="img-thumb">
+                <img src="./thumbnail.jpg" alt="" />
+                <p className="time">{props.time}</p>
+            </div>
+            <p className="title">{props.title}</p>
+            <p className="desc">{props.desc}</p>
+        </div>
+    )
+}
+
+export default YouTubeComp;
